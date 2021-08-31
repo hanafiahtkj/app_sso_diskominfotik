b7b6d55a936788affa1804b07fa3d7c9439d32af
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-08-31 14:21:01 +0700

    #

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@ $user = auth()->user();
 @endphp
 
 <div class="navbar-bg bg-transparent"></div>
-<nav class="navbar navbar-expand-lg main-navbar">
+<nav class="navbar navbar-expand-lg main-navbar fixed-top" style="background-color: #f4f6f9;">
 <div class="container">
     <!-- <a href="{{ url('') }}" class="navbar-brand d-none d-md-none"><img src="{{ asset('img/logo.png') }}" width="225" height="51" class="d-inline-block align-top" alt=""></a> -->
     <a class="navbar-brand sidebar-gone-hide" href="{{ url('') }}"><img src="{{ asset('img/logo.png') }}" width="202" height="51" class="d-inline-block align-top" alt=""></a>
