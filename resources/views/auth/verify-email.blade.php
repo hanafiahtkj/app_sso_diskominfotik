404a7b3e30b71adf02c0f664e9c1b1c40ec0c47b
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-09-09 14:24:06 +0800

    #

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -20,7 +20,7 @@
 
                 <div>
                     <x-jet-button type="submit">
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Kirim Ulang Email untuk Verifikasi') }}
                     </x-jet-button>
                 </div>
             </form>
