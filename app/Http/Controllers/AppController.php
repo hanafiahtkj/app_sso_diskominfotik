57c4384984f59eee907c9f3924c752ba6fc8cf4d
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-09-12 07:52:44 +0800

    @

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -31,7 +31,7 @@ class AppController extends Controller
     {
         $data = [
             'app' => null,
-            'kategori' => Kategori::all(),
+            'kategori' => Kategori::orderBy('urut', 'asc')->get(),
         ];
         return view('pages.app.app-form', $data);
     }
