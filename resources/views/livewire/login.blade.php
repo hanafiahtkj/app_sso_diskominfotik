8d1e92350a07da7dd0f4dada0f9202dc470107f5
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-10-08 16:49:57 +0800

    #

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -15,7 +15,7 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ url('/login').((request()->get('redirect')) ? '?redirect='.request()->get('redirect') : '' ) }}">
             @csrf
 
             <div>
