efd6539c5180a8a97b7ac17c5f87d377bcdf4116
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-10-11 14:57:23 +0800

    #

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <div style="text-align:center;">
     <a href="{{ url('/') }}"><img src="{{ asset('img/logo2.png') }}" height="51" class="d-inline-block align-top" alt="" style="
-        height: 100px;
+        height: 70px;
     "></a>
 </div>
