883c661ee66e572d0f290d6d8cf5d99aae304b8a
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-11-11 14:55:58 +0800

    #

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -6,7 +6,7 @@ return [
         'name' => env('APP_NAME', 'Banjarmasin Dalam Genggaman'),
         'short_name' => 'Banjarmasin Dalam Genggaman',
         'start_url' => '/',
-        'background_color' => '#ffffff',
+        'background_color' => '#2969b0',
         'theme_color' => '#000000',
         'display' => 'standalone',
         'orientation'=> 'any',
@@ -56,22 +56,6 @@ return [
             '1668x2224' => '/images/icons/splash-1668x2224.png',
             '1668x2388' => '/images/icons/splash-1668x2388.png',
             '2048x2732' => '/images/icons/splash-2048x2732.png',
-        ],
-        'shortcuts' => [
-            [
-                'name' => 'Shortcut Link 1',
-                'description' => 'Shortcut Link 1 Description',
-                'url' => '/shortcutlink1',
-                'icons' => [
-                    "src" => "/images/icons/icon-72x72.png",
-                    "purpose" => "any"
-                ]
-            ],
-            [
-                'name' => 'Shortcut Link 2',
-                'description' => 'Shortcut Link 2 Description',
-                'url' => '/shortcutlink2'
-            ]
         ],
         'custom' => []
     ]
