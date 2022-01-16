65acfcda2d48e47dcdaa76efb47c2bec678bf3cd
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2022-01-16 14:38:06 +0800

    #

--- resources/views/livewire/apps.blade.php
+++ resources/views/livewire/apps.blade.php
@@ -7,21 +7,18 @@
 
     <div class="overflow-hidden sm:rounded-lg">
         <div class="row" id="app-kategori">
-            @foreach ($kategori as $kat)
+            {{-- @foreach ($kategori as $kat)
             <div class="col-6 col-sm-4 col-md-4 col-lg-3">
                 <a class="onTap" href="{{ url('app/'.$kat->id) }}">
                     <article class="article">
                         <div class="article-header">
                             <div class="article-image" data-background="url('{{ isset($kat->path) ? asset(Storage::url($kat->path)) : '' }}')" style="background-size: contain; background-image: url(&quot;{{ isset($kat->path) ? asset(Storage::url($kat->path)) : '' }}&quot;);">
                             </div>
-                            {{-- <div class="article-title text-white">
-                                <h5>{{ $app->keterangan }}</h5>
-                            </div> --}}
                         </div>
                     </article>
                 </a>
             </div>
-            @endforeach
+            @endforeach --}}
         </div>
     </div>
 
