f064cb1adfac81bb94e56f116a1a21a77b5d8067
Deny298 <94414853+Deny298@users.noreply.github.com>
2022-01-19 20:49:53 +0800

    #

--- resources/views/livewire/apps.blade.php
+++ resources/views/livewire/apps.blade.php
@@ -11,9 +11,12 @@
             <div class="col-6 col-sm-4 col-md-4 col-lg-3">
                 <a class="onTap" href="{{ url('app/'.$kat->id) }}">
                     <article class="article">
-                        <div class="article-header">
+                        <div class="article-header p-2 pt-3">
                             <div class="article-image" data-background="url('{{ isset($kat->path) ? asset(Storage::url($kat->path)) : '' }}')" style="background-size: contain; background-image: url(&quot;{{ isset($kat->path) ? asset(Storage::url($kat->path)) : '' }}&quot;);">
                             </div>
+                        </div>
+                        <div class="article-details line-clamp px-3 pt-0 pb-1 d-flex align-items-center justify-content-center">
+                            {{ $kat->keterangan }}
                         </div>
                     </article>
                 </a>
